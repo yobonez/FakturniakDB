761196051a0953cba54d9fa1c02301342c7772dc
yobonez <59935203+yobonez@users.noreply.github.com>
2022-07-11 10:05:52 +0200

    Kolejne zmiany w bazie
    - Zaimplementowałem generator numerów faktur w procedurze tworzenia
      faktury
    - Teraz pliki .sql powinny się prawidłowo wyświetlać w diff'Kolejne zmiany w bazie

---
+++ dbo.vFaktury.View.sql
@@ -0,0 +1,19 @@
+USE [FakturniakDB]
+GO
+/****** Object:  View [dbo].[vFaktury]    Script Date: 11.07.2022 10:03:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jacek Ga³uszka
+-- Create date: 11.07.2022
+-- Description:	Widok faktur
+-- =============================================
+
+CREATE VIEW [dbo].[vFaktury] AS
+
+SELECT f.numer_faktury, typy.opis AS 'Typ faktury', sp.imie AS 'Sprzedawca', sp.nazwisko AS ' ', nab.imie AS 'Nabywca', nab.nazwisko AS '.', s.nazwa AS 'Sposób p³atnoœci', f.termin_platnosci 
+FROM faktury f, kontrahenci sp, kontrahenci nab, sposoby_platnosci s, typy_faktur typy
+WHERE f.id_sprzedawca = sp.id_kontrahenta AND f.id_nabywca = nab.id_kontrahenta AND f.id_sposob_platnosci = s.id_sposob_platnosci AND f.id_typu_faktury = typy.id_typu_faktury
+GO
---
+++ dbo.vMtmFaktury.View.sql
@@ -0,0 +1,13 @@
+USE [FakturniakDB]
+GO
+/****** Object:  View [dbo].[vMtmFaktury]    Script Date: 11.07.2022 10:03:38 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[vMtmFaktury] AS
+
+SELECT f.numer_faktury, p.nazwa AS 'Produkt', p.cena_netto, st.wartosc AS 'Stawka VAT',p.cena_brutto, mtmp.ilosc AS 'Iloœæ', jdn.nazwa AS 'Jednostka', p.cena_brutto * mtmp.ilosc AS 'Suma' 
+FROM dbo.faktury f, dbo.produkty p, dbo.mtm_faktura_produkty mtmp, dbo.stawki_vat st, jednostki_miary jdn
+WHERE f.numer_faktury = mtmp.numer_faktury AND p.id_produktu = mtmp.id_produktu AND p.id_stawki = st.id_stawki AND jdn.id_jednostki = p.id_jednostki
+GO
